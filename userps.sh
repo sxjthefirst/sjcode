8468632aacb9e7de5422a36f1e16dc2b9ee78605
Sid <sxjthefirst@gmail.com>
2018-09-10 14:24:43 +1000

    Added per process command

--- userps.sh
+++ userps.sh
@@ -1,4 +1,6 @@
 #sxjthefirst / userps.sh
 # Get memory used by each user. Tested on RHEL 5.x, 6.x and Mint Nadia
-
 ps -eo rss,user | grep -v USER | awk '{ c=split($2,arr," "); cmd=arr[c]; cmdcnt[cmd]++; cmdrss[cmd] += $1 } END { for (i in cmdrss) print cmdrss[i]/(1024*1024), i, cmdcnt[i] }' | sort -nr | awk '{printf "%60-s %4i Processes %6.2f GB \n", $2, $3, $1}'
+
+# Get memory used by each process. Tested on  6.x
+ps -eo rss,cmd | grep -v CMD | awk '{ c=split($2,arr," "); cmd=arr[c]; cmdcnt[cmd]++; cmdrss[cmd] += $1 } END { for (i in cmdrss) print cmdrss[i]/(1024*1024), i, cmdcnt[i] }' | sort -nr | awk '{printf "%60-s %4i Processes %6.2f GB \n", $2, $3, $1}'
